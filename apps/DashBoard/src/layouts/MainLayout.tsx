55af01e11f14b6164e5939133098e84106e3d7de
xiaolv <sunmozong@gmail.com>
2025-01-30 04:35:08 +0800

    refactor(dashboard): Extract MainLayout into separate component file

---
+++ apps/DashBoard/src/layouts/MainLayout.tsx
@@ -0,0 +1,48 @@
+import { Layout } from 'antd'
+import { Outlet } from '@tanstack/react-router'
+import SiderMenu from '../container/Menu'
+
+const { Header, Sider, Content } = Layout
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 24,
+  lineHeight: '64px',
+  backgroundColor: '#001529',
+};
+
+const contentStyle: React.CSSProperties = {
+  padding: 24,
+  minHeight: 'calc(100vh - 128px)',
+  backgroundColor: '#fff',
+  color: '#000',
+  flex: 1,
+};
+
+const siderStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#001529',
+  height: '100vh',
+  overflow: 'auto',
+};
+
+const containerStyle: React.CSSProperties = {
+  minHeight: '100vh',
+  margin: 0,
+  padding: 0,
+};
+
+export const MainLayout = () => (
+  <Layout style={containerStyle}>
+    <Sider width={200} style={siderStyle} breakpoint="lg" collapsedWidth="0">
+      <SiderMenu />
+    </Sider>
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Content style={contentStyle}><Outlet /></Content>
+    </Layout>
+  </Layout>
+) 
